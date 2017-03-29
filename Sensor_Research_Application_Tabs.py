181153b442bc895aa04ee196b70313d405a6c500
tawender <tawender@customsiliconsolutions.com>
2017-03-29 02:30:18 -0700

    gui widgets placed into tabs

--- Sensor_Research_Application_Tabs.py
+++ Sensor_Research_Application_Tabs.py
@@ -1,7 +1,7 @@
 from Tkinter import *
 import ttk
 import tkFont
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 from ConfigParser import ConfigParser
 
 
@@ -10,8 +10,8 @@ from ConfigParser import ConfigParser
 fixture_GPIO_ports = [4,17,22]
 
 #setup GPIO for selecting a test fixture to monitor
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(fixture_GPIO_ports, GPIO.OUT, initial=GPIO.HIGH)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(fixture_GPIO_ports, GPIO.OUT, initial=GPIO.HIGH)
 
 #screen layout
 num_MFCs = 6
@@ -22,46 +22,32 @@ class App:
     def __init__(self):
         self.root = Tk()
 
-        self.create_menu()
-        self.create_main_panel()
+        self.mainPanel = Frame(self.root)
+        self.mainPanel.grid(sticky=N)
+
+        self.tabs = ttk.Notebook(self.mainPanel)
+        self.tabs.grid(row=0,column=0)
+        self.tab1_frame = Frame(self.tabs)
+        self.tab2_frame = Frame(self.tabs)
+
         self.create_fixture_select_section()
         self.create_test_configuration_section()
         self.create_fixture_monitoring_section()
         self.create_exit_button()
 
+        self.tabs.add(self.tab1_frame,text='Fixture Control')
+        self.tabs.add(self.tab2_frame,text='Test Condition Setup')
+
+        self.create_menu()
 
         self.root.title("NEA Sensor Research")
         self.root.geometry('1300x550')
         self.root.mainloop()
-		
-		
-    def create_main_panel(self):
-		
-        mainPanel = Frame(self.root)
-        mainPanel.grid(sticky=N,row=)
 
-        self.tabs = ttk.Notebook(mainPanel)
-        self.tabs.grid(row=0,column=0)
-
-    def create_menu(self):
-
-        menubar = Menu(self.root)
-
-        filemenu = Menu(menubar,tearoff=0)
-        filemenu.add_command(label="Save")
-        filemenu.add_command(label="Save as...")
-        filemenu.add_separator()
-        filemenu.add_command(label="Exit",command=self.exitProgram)
-        menubar.add_cascade(label="File",menu=filemenu)
-
-        helpmenu = Menu(menubar,tearoff=0)
-        helpmenu.add_command(label="About...")
-        menubar.add_cascade(label="Help",menu=helpmenu)
-
-        self.root.config(menu=menubar)
 
 
     def create_fixture_select_section(self):
+
         #create a list of tuples used to build radio buttons for fixture selection
         i = 1
         self.fixture_selections = []
@@ -76,12 +62,12 @@ class App:
         self.fixture_selection.set(0)
 
         #create the border grouping all fixture selection radio buttons
-        fixture_selection_frame = LabelFrame(self.root,text="Test Fixture Selection",padx=10,pady=10)
-        fixture_selection_frame.grid(row=1,column=0,padx=10,pady=10,sticky=W)
+        fixture_selection_border = LabelFrame(self.tab1_frame,text="Test Fixture Selection",padx=10,pady=10)
+        fixture_selection_border.grid(row=0,column=0,padx=10,pady=10,sticky=W)
 
         #create fixture selection radio buttons
         for lbl, portNum in self.fixture_selections:
-        	Radiobutton(fixture_selection_frame,
+        	Radiobutton(fixture_selection_border,
         				text=lbl,
         				padx=5,
         				variable=self.fixture_selection,
@@ -90,7 +76,7 @@ class App:
 
 
     def create_test_configuration_section(self):
-        mfc_control_frame = LabelFrame(self.root,text="Test Configuration")
+        mfc_control_frame = LabelFrame(self.tab2_frame,text="Test Configuration")
         mfc_control_frame.grid(row=0,column=0,padx=10,pady=10,ipady=5,columnspan=2)
 
         mfc_checkboxes = []
@@ -135,26 +121,42 @@ class App:
 
     def create_fixture_monitoring_section(self):
 		#create chip fixture monitor section
-		chip_fixture_monitor_frame = Frame(self.tabs,name='fixturemonitor')
+        chip_fixture_monitor_frame = LabelFrame(self.tab1_frame,text="PHT Sensor Measurements")
+        chip_fixture_monitor_frame.grid(row=0,column=1)
 
-		PHT_sensor_font = tkFont.Font(family="Helvetica",size=11)
-		Label(chip_fixture_monitor_frame,text="Temperature:",font=PHT_sensor_font).grid(row=0,column=0,sticky=E)
-		Label(chip_fixture_monitor_frame,text="25.1",font=PHT_sensor_font,borderwidth=2,relief='sunken',width=6).grid(row=0,column=1)
-		Label(chip_fixture_monitor_frame,text="*C",font=PHT_sensor_font).grid(row=0,column=2,sticky=W)
-		Label(chip_fixture_monitor_frame,text="Humidity:",font=PHT_sensor_font).grid(row=1,column=0,sticky=E)
-		Label(chip_fixture_monitor_frame,text="45",font=PHT_sensor_font,borderwidth=2,relief='sunken',width=6).grid(row=1,column=1)
-		Label(chip_fixture_monitor_frame,text="%RH",font=PHT_sensor_font).grid(row=1,column=2,sticky=W)
-		Label(chip_fixture_monitor_frame,text="Pressure:",font=PHT_sensor_font).grid(row=2,column=0,sticky=E)
-		Label(chip_fixture_monitor_frame,text="1000.5",font=PHT_sensor_font,borderwidth=2,relief='sunken',width=6).grid(row=2,column=1)
-		Label(chip_fixture_monitor_frame,text="mbar",font=PHT_sensor_font).grid(row=2,column=2,sticky=W)
-
-		self.tabs.add(chip_fixture_monitor_frame,text='Fixture Monitoring')
+        PHT_sensor_font = tkFont.Font(family="Helvetica",size=11)
+        Label(chip_fixture_monitor_frame,text="Temperature:",font=PHT_sensor_font).grid(row=0,column=0,sticky=E)
+        Label(chip_fixture_monitor_frame,text="25.1",font=PHT_sensor_font,borderwidth=2,relief='sunken',width=6).grid(row=0,column=1)
+        Label(chip_fixture_monitor_frame,text="*C",font=PHT_sensor_font).grid(row=0,column=2,sticky=W)
+        Label(chip_fixture_monitor_frame,text="Humidity:",font=PHT_sensor_font).grid(row=1,column=0,sticky=E)
+        Label(chip_fixture_monitor_frame,text="45",font=PHT_sensor_font,borderwidth=2,relief='sunken',width=6).grid(row=1,column=1)
+        Label(chip_fixture_monitor_frame,text="%RH",font=PHT_sensor_font).grid(row=1,column=2,sticky=W)
+        Label(chip_fixture_monitor_frame,text="Pressure:",font=PHT_sensor_font).grid(row=2,column=0,sticky=E)
+        Label(chip_fixture_monitor_frame,text="1000.5",font=PHT_sensor_font,borderwidth=2,relief='sunken',width=6).grid(row=2,column=1)
+        Label(chip_fixture_monitor_frame,text="mbar",font=PHT_sensor_font).grid(row=2,column=2,sticky=W)
 
     def create_exit_button(self):
 		#create button used to exit the application
-		exitButton = Button(self.root, text="Exit", command=self.exitProgram, height=2,width=10).grid()
+		exitButton = Button(self.mainPanel, text="Exit", command=self.exitProgram, height=2,width=10).grid()
 
 
+
+    def create_menu(self):
+
+        menubar = Menu(self.root)
+
+        filemenu = Menu(menubar,tearoff=0)
+        filemenu.add_command(label="Save")
+        filemenu.add_command(label="Save as...")
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit",command=self.exitProgram)
+        menubar.add_cascade(label="File",menu=filemenu)
+
+        helpmenu = Menu(menubar,tearoff=0)
+        helpmenu.add_command(label="About...")
+        menubar.add_cascade(label="Help",menu=helpmenu)
+
+        self.root.config(menu=menubar)
 
     #-----------------------------------------------------------
     #----------------function definitions-----------------------
